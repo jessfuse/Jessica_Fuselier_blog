315577be411bd7658b547e2d9b1b1ef2a65cbbfb
Jessica Fuselier <jessfuse@gmaill.com>
2013-07-01 12:02:34 -0500

    added new.html.haml

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,12 @@ class PostsController < ApplicationController
     @name = "Jessica"
     @posts = Post.all 
 	end 
+  
   def show 
     @post = Post.find(params[:id])
   end
+  
+  def new
+    @post = Post.new
+  end 
 end 
