4122d8cadc1ebe4445ea4988d14ad743bf63ca40
Jessica Fuselier <jessfuse@gmaill.com>
2013-07-01 09:42:08 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JessicaFuselierBlog::Application.config.secret_token = '81d6de151954c8233eba349a3623d7228a664de3aace2c47756a5ca3c27b6ee50b5ddef674bedcb550871bf15dd496832df7079c404a0aab73dd2ef5d154a5a2'
